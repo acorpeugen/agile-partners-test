51e3a4a62b1007b7361035fddff17ca23aaf9bee
Eugene Proca <proca.1987@gmail.com>
2020-11-20 18:31:25 +0200

    Add new images and svg files

---
+++ src/assets/images/index.ts
@@ -0,0 +1,11 @@
+export { default as section } from './section.jpg';
+export { default as sectionXl } from './section-xl.jpg';
+export { default as sectionLg } from './section-lg.jpg';
+export { default as sectionMd } from './section-md.jpg';
+export { default as sectionSm } from './section-sm.jpg';
+
+export { default as footer } from './footer.jpg';
+export { default as footerXl } from './footer-xl.jpg';
+export { default as footerLg } from './footer-lg.jpg';
+export { default as footerMd } from './footer-md.jpg';
+export { default as footerSm } from './footer-sm.jpg';
---
+++ src/components/ArrowDownSvg/ArrowDownSvg.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ArrowDownStyled = styled.svg`
+  margin-bottom: 88px;
+`;
+
+export interface ArrowDownSvgProps {
+  width?: number;
+  height?: number;
+  fill?: string;
+}
+
+const ArrowDownSvg = ({
+  width = 12,
+  height = 24,
+  fill = '#fff',
+}: ArrowDownSvgProps) => {
+  return (
+    <ArrowDownStyled
+      fill="none"
+      {...{ width, height }}
+      viewBox="0 0 12 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="m6 24 1-1 5-5-1.4-1.4-3.6 3.6v-20.20000022l-2-.00000009v20.20000031l-3.6-3.6-1.40000079 1.4 5.00000079 5z"
+        {...{ fill }}
+      />
+    </ArrowDownStyled>
+  );
+};
+
+export default ArrowDownSvg;
