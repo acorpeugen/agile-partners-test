c95057523473daf8adf462831fe6a3b584071f30
Eugene Proca <proca.1987@gmail.com>
2020-11-20 18:40:01 +0200

    Create App.tsx file.
    Add Preloader component and animation for him.
    Add Router

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { gsap } from 'gsap';
+import { Home, Preloader } from './components';
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    // Preloader and loader GSAP animation
+    const preloader = document.querySelector('.preloader-js');
+    const loader = document.querySelector('.loader-js');
+
+    const preloaderAnimation = gsap.timeline();
+
+    preloaderAnimation.to(
+      [preloader, loader],
+      { opacity: 0, duration: 1 },
+      '+=1'
+    );
+  }, []);
+
+  return (
+    <>
+      {isLoading ? (
+        <Preloader />
+      ) : (
+        <Router>
+          <Switch>
+            <Route path="/" render={() => <Home />} />
+          </Switch>
+        </Router>
+      )}
+    </>
+  );
+}
+
+export default App;
