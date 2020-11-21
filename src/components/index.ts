f09a12aa4abe196d666552b45ba2f853eed8d662
Eugene Proca <proca.1987@gmail.com>
2020-11-21 09:46:52 +0200

    Create index.ts
    Add component exports.

---
+++ src/components/index.ts
@@ -0,0 +1,12 @@
+export { Seo } from './Seo';
+export { Preloader } from './Preloader';
+export { Container } from './Container';
+export { Header } from './Header';
+export { Footer, FooterNav } from './Footer';
+
+export { Image } from './Image';
+export { LineSvg } from './LineSvg';
+export { ArrowDownSvg } from './ArrowDownSvg';
+
+export { Home } from './Home';
+export { Banks } from './Banks';
