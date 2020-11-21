fdfc8b067a55f6b3fb4dcd5278f539cbb17f0ed9
Eugene Proca <proca.1987@gmail.com>
2020-11-21 17:18:16 +0200

    Update Seo.txs
    Add meta description

--- src/components/Seo/Seo.tsx
+++ src/components/Seo/Seo.tsx
@@ -10,6 +10,7 @@ function Seo({ title, description }: SeoProps) {
   return (
     <Helmet>
       <title>{title}</title>
+      <meta name="description" content={description}></meta>
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
     </Helmet>
