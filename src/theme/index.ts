d9509c9146fa52ce6593bf13b8366e24d6314635
Eugene Proca <proca.1987@gmail.com>
2020-11-21 23:21:39 +0200

    Update src/theme/index.ts
    Add new export function fluidFontSize

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,3 +1,4 @@
 export { default as Theme } from './theme';
 export { default as GlobalStyle } from './globalStyle';
 export { H1, H2, H3, H4 } from './typography';
+export { fluidFontSize } from './mixins';
