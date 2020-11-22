617b42f3ea1220caa94f3d17b60ccf942cd0880a
Eugene Proca <proca.1987@gmail.com>
2020-11-22 02:05:59 +0200

    Update index.ts
    Move typograpth

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,4 +1,4 @@
 export { default as Theme } from './theme';
 export { default as GlobalStyle } from './globalStyle';
-export { H1, H2, H3, H4 } from './typography';
 export { fluidFontSize } from './mixins';
+export { H1, H2, H3, H4 } from './typography';
