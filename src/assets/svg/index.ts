907e5777476db0f6670f1f167baf2b723f91b121
Eugene Proca <proca.1987@gmail.com>
2020-11-22 02:00:34 +0200

    Update index.js
    Add default exports

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -1,1 +1,7 @@
+export { default as arrowDown } from './arrow-down.svg';
 export { default as arrowRight } from './arrow-right.svg';
+export { default as burger } from './burger.svg';
+export { default as favicon } from './favicon.svg';
+export { default as line } from './line.svg';
+export { default as logo } from './logo.svg';
+export { default as map } from './map.svg';
