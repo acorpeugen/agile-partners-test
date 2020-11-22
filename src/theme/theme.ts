c64a1d961b7f25bd8f8498d8b1d79d83b1c2cb6d
Eugene Proca <proca.1987@gmail.com>
2020-11-22 02:06:39 +0200

    Update theme.ts
    Add in theme breakpoints down and zIndex

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -13,12 +13,20 @@ const theme = {
     up_768: '768px',
     up_992: '992px',
     up_1200: '1200px',
+    down_575: '575px',
+    down_767: '767px',
+    down_991: '991px',
+    down_1199: '1199px',
   },
   container: {
     mw_540: '540px',
     mw_720: '720px',
     mw_960: '960px',
     mw_1140: '1140px',
+  },
+  zIndex: {
+    sticky: '1020',
+    fixed: '1030',
   },
 };
 
