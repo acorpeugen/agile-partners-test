134e6a395f28522033bce6998ad35d5bd3de6765
Eugene Proca <proca.1987@gmail.com>
2020-11-22 02:34:05 +0200

    Update typography.ts
    Change styles.

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -2,7 +2,11 @@ import styled from 'styled-components';
 
 export const H1 = styled.h1`
   color: ${(props) => props.theme.colors.white};
-  margin-bottom: 60px;
+  margin-bottom: 30px;
+
+  @media (min-width: ${(props) => props.theme.breakpoint.up_992}) {
+    margin-bottom: 60px;
+  }
 `;
 
 export const H2 = styled.h2`
